a10b7563774c3c2081b86550b6d2c4066d976b9a
anavics02 <102831725+anavics02@users.noreply.github.com>
2022-04-01 14:48:16 -0700

    click

--- MyApp.py
+++ MyApp.py
@@ -1,15 +1,16 @@
 import streamlit as st
 st.title("MI PRIMER APP")
-#st.button("dale click")
+click=st.button("dale click")
+st.write("el valor de click es ",click)
 #st.button("otro botón")
 import pandas as pd
 #las siguientes tres lineas eran para elprimer ejercicio
 df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
 #st.write(df)
 #st.map(df)
-st.text("HOLA MUNDO")
-st.text("la sigiente es una integral")
-st.latex(" \int ")
-st.markdown(" #titulo ")
-st.markdown(" *este es una viñeta* ")
-st.markdown(" **negritas** ")
+#st.text("HOLA MUNDO")
+#st.text("la sigiente es una integral")
+#st.latex(" \int ")
+#st.markdown(" #titulo ")
+#st.markdown(" *este es una viñeta* ")
+#st.markdown(" **negritas** ")
