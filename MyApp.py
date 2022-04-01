ecd5b4f7c1d8145e1b64e259a2031989c55b3c99
anavics02 <102831725+anavics02@users.noreply.github.com>
2022-04-01 14:21:24 -0700

    x

--- MyApp.py
+++ MyApp.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.title("MI PRIMER APP")
 #st.button("dale click")
 #st.button("otro botón")
+import pandas as pd
+df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
+st.write(df)
+st.map(df)
